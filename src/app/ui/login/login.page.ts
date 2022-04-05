e9f5a8cfc7997ff1ded422eec591a05ca75ba5ed
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-05 23:31:34 +0300

    create login form and email errors

--- src/app/ui/login/login.page.ts
+++ src/app/ui/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private formBuilder: FormBuilder) { }
+  isLoad = true;
+  loginForm: FormGroup
   ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.email, Validators.required, Validators.maxLength(50)]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  login(){
+    this.isLoad = false;
+    setTimeout(() => {
+      this.isLoad = true;
+    }, 2000);
   }
 
 }
