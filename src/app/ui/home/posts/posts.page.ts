a7655a33699565762df8046b820a31a1d7451bed
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 14:18:20 +0300

    animations

--- src/app/ui/home/posts/posts.page.ts
+++ src/app/ui/home/posts/posts.page.ts
@@ -7,16 +7,44 @@ declare var $: any;
 })
 export class PostsPage implements OnInit {
 
-  isClickHeart:boolean=false;
-  isClickBookmark:boolean=false;
+  isClickHeart: boolean = false;
+  isClickBookmark: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
 
-  postLike() {
-    if(this.isClickHeart) this.isClickHeart = false
-    else this.isClickHeart = true;
+  postLike(id: string) {
+    if (this.isClickHeart) {
+      this.isClickHeart = false;
+      setTimeout(() => {
+        $("#postlike" + id + "_1").animate({
+          fontSize: "30px",
+          opacity: 0
+        }, 0)
+        setTimeout(() => {
+          $("#postlike" + id + "_1").animate({
+            fontSize: "30px",
+            opacity: 1
+          })
+        }, 200);
+      }, 100);
+    }
+    else {
+      this.isClickHeart = true;
+      setTimeout(() => {
+        $("#postlike" + id + "_2").animate({
+          fontSize: "30px",
+          opacity: 0
+        }, 0)
+        setTimeout(() => {
+          $("#postlike" + id + "_2").animate({
+            fontSize: "30px",
+            opacity: 1
+          })
+        }, 200);
+      }, 100);
+    }
   }
 
   postComment() {
@@ -27,9 +55,33 @@ export class PostsPage implements OnInit {
 
   }
 
-  postSave() {
-    if(this.isClickBookmark) this.isClickBookmark = false
-    else this.isClickBookmark = true;
+  postSave(id: string) {
+    if (this.isClickBookmark) {
+      this.isClickBookmark = false;
+      setTimeout(() => {
+        $("#postsave" + id + "_1").animate({
+          opacity: 0
+        }, 0)
+        setTimeout(() => {
+          $("#postsave" + id + "_1").animate({
+            opacity: 1
+          })
+        }, 200);
+      }, 100);
+    }
+    else {
+      this.isClickBookmark = true;
+      setTimeout(() => {
+        $("#postsave" + id + "_2").animate({
+          opacity: 0
+        }, 0)
+        setTimeout(() => {
+          $("#postsave" + id + "_2").animate({
+            opacity: 1
+          })
+        }, 200);
+      }, 100);
+    }
   }
 
 }
