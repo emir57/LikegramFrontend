ce1ebc4a7868ac6169a629a0754b7edb9081f067
emir57 <emir.gurbuz06@hotmail.com>
2022-06-15 15:19:43 +0300

    likeOrUnlike function

--- src/app/services/comment-like.service.ts
+++ src/app/services/comment-like.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { ResponseModel } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentLikeService {
 
-  constructor() { }
+  constructor(
+    @Inject("baseUrl") private baseUrl: string,
+    private http: HttpClient
+  ) { }
+
+  likeOrUnlike(commentId: number, userId: number) {
+    let newUrl = `${this.baseUrl}api/commentlikes/likeorunlike`;
+    return this.http.post<ResponseModel>(newUrl, {
+      commentId: commentId,
+      userId: userId
+    })
+  }
+
+  checkLike(commentId: number, userId: number) {
+
+  }
 }
