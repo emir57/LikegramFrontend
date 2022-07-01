c5df7557db9d6832ef1fbee15a56ff6ddd1f41de
emir57 <emir.gurbuz06@hotmail.com>
2022-07-01 15:41:13 +0300

    post module export and declare PostComponent

--- src/app/ui/home/posts/post/post.module.ts
+++ src/app/ui/home/posts/post/post.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PostComponent } from './post.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PostComponent
+  ],
   imports: [
     CommonModule
+  ],exports:[
+    PostComponent
   ]
 })
 export class PostModule { }
