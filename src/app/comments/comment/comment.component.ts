cd6de72d39fb2c780f9d28536e9d48eaeb78f0a9
emir57 <emir.gurbuz06@hotmail.com>
2022-07-01 16:30:58 +0300

    getUser

--- src/app/comments/comment/comment.component.ts
+++ src/app/comments/comment/comment.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PostCommentModel } from 'src/app/models/postCommentModel';
+import { UserModel } from 'src/app/models/userModel';
+import { CommentLikeService } from 'src/app/services/comment-like.service';
+import { KeyType, StorageService } from 'src/app/services/storage.service';
+declare var $: any;
 
 @Component({
   selector: 'app-comment',
@@ -9,8 +13,71 @@ import { PostCommentModel } from 'src/app/models/postCommentModel';
 export class CommentComponent implements OnInit {
 
   @Input() comment: PostCommentModel
-  constructor() { }
+  user: UserModel;
+  constructor(
+    private commentLikeService: CommentLikeService,
+    private storageService: StorageService
+  ) { }
 
-  ngOnInit() { }
+  async ngOnInit() {
+    await this.getUser();
+  }
+
+  async getUser() {
+    this.user = JSON.parse(await this.storageService.getValue(KeyType.User));
+  }
+
+  commentLike(comment: PostCommentModel) {
+    let fontSize = "20px";
+    this.commentLikeService.likeOrUnlike(comment.id, this.user.id).subscribe(async response => {
+      if (response.success) {
+        if (comment.isClickHeart) {
+          comment.isClickHeart = false;
+          setTimeout(() => {
+            $("#commentlike" + comment.id + "_1").animate({
+              fontSize: fontSize,
+              opacity: 0
+            }, 0)
+            setTimeout(() => {
+              $("#commentlike" + comment.id + "_1").animate({
+                fontSize: fontSize,
+                opacity: 1
+              })
+            }, 200);
+          }, 100);
+        }
+        else {
+          comment.isClickHeart = true;
+          setTimeout(() => {
+            $("#commentlike" + comment.id + "_2").animate({
+              fontSize: fontSize,
+              opacity: 0
+            }, 0)
+            setTimeout(() => {
+              $("#commentlike" + comment.id + "_2").animate({
+                fontSize: fontSize,
+                opacity: 1
+              })
+            }, 200);
+          }, 100);
+        }
+      }
+    })
+  }
+
+  selectComment(comment: PostCommentModel) {
+    if (this.selectedComment) this.removeSelectedComment();
+    $("#comment" + comment.id).addClass("bg-warning text-white");
+    this.selectedComment = comment;
+  }
+  removeSelectedComment() {
+    $("#comment" + this.selectedComment.id).removeClass("bg-warning text-white");
+    this.selectedComment = undefined;
+  }
+
+  getDate(dateString: string) {
+    let date = new Date(dateString);
+    return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`
+  }
 
 }
