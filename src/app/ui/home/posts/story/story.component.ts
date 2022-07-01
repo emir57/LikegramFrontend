7834672a3cf6334c84c3c29eb83ce532ebd85666
emir57 <emir.gurbuz06@hotmail.com>
2022-07-01 16:51:42 +0300

    story component change selector name

--- src/app/ui/home/posts/story/story.component.ts
+++ src/app/ui/home/posts/story/story.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-story',
+  selector: 'story-card',
   templateUrl: './story.component.html',
   styleUrls: ['./story.component.scss'],
 })
@@ -9,6 +9,6 @@ export class StoryComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
