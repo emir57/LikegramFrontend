d5ed389a535d0e696c308b5027804d1b07926634
emir57 <emir.gurbuz06@hotmail.com>
2022-07-01 17:59:53 +0300

    your-story -> scale animation

--- src/app/ui/home/posts/story/story.component.ts
+++ src/app/ui/home/posts/story/story.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'story-card',
@@ -13,5 +14,13 @@ export class StoryComponent implements OnInit {
   ) { }
 
   ngOnInit() { }
+
+  shareStory() {
+    const your_story = $("#your-story");
+    your_story.css("transform", "scale(.8)");
+    setTimeout(() => {
+      your_story.css("transform", "scale(1)");
+    }, 200);
+  }
 
 }
