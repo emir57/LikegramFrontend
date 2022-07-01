6452c354962a3c02c79279acbceef9c638069603
emir57 <emir.gurbuz06@hotmail.com>
2022-07-01 15:44:27 +0300

    inject services

--- src/app/ui/home/posts/post/post.component.ts
+++ src/app/ui/home/posts/post/post.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { PostModel } from 'src/app/models/postModel';
+import { FavouritePostService } from 'src/app/services/favourite-post.service';
+import { PostLikeService } from 'src/app/services/post-like.service';
+import { PostService } from 'src/app/services/post.service';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-post',
@@ -7,8 +13,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  @Input() post: PostModel;
+  constructor(
+    private storageService: StorageService,
+    private postService: PostService,
+    @Inject("baseUrl") public baseUrl: string,
+    private modalController: ModalController,
+    private postLikeService: PostLikeService,
+    private favouritePostService: FavouritePostService
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
