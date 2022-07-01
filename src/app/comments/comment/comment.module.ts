28720e51ad1e9a97b7f33958de811d027dc5553d
emir57 <emir.gurbuz06@hotmail.com>
2022-07-01 16:29:19 +0300

    comment module import IonicModule

--- src/app/comments/comment/comment.module.ts
+++ src/app/comments/comment/comment.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommentComponent } from './comment.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
@@ -9,7 +10,8 @@ import { CommentComponent } from './comment.component';
     CommentComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ], exports: [
     CommentComponent
   ]
