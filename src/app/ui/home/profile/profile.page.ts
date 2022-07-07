832e11f676481618975006508c52801334026655
emir57 <emir.gurbuz06@hotmail.com>
2022-07-07 14:23:27 +0300

    postCount

--- src/app/ui/home/profile/profile.page.ts
+++ src/app/ui/home/profile/profile.page.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { UserModel } from 'src/app/models/userModel';
+import { PostService } from 'src/app/services/post.service';
 import { KeyType, StorageService } from 'src/app/services/storage.service';
 
 @Component({
@@ -9,17 +10,27 @@ import { KeyType, StorageService } from 'src/app/services/storage.service';
 })
 export class ProfilePage implements OnInit {
 
-  user: UserModel
+  user: UserModel;
+  postCount: number = 0;
   constructor(
     @Inject("baseUrl") public baseUrl: string,
-    private storageService: StorageService
+    private storageService: StorageService,
+    private postService: PostService
   ) { }
 
   async ngOnInit() {
     await this.getUser();
+    this.getPostCount();
   }
   async getUser() {
     this.user = JSON.parse(await this.storageService.getValue(KeyType.User));
+  }
+
+  getPostCount() {
+    this.postService.getPostCount(this.user.id).subscribe(async response => {
+      if (response.success)
+        this.postCount = response.data;
+    })
   }
 
   openMenu() {
